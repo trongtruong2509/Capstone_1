2c782ea3e1f220c4818e151b8772b339ce52405f
lamtt <lamtt7777@gmail.com>
2022-06-16 17:13:45 +0700

    Update

--- js/index.js
+++ js/index.js
@@ -3,19 +3,36 @@ const navContainer = document.querySelector(".page-header nav .container");
 // const menu = document.querySelector(".page-header .menu");
 
 let lastScroll = 0;
- 
+
 window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
- 
-  if (currentScroll > lastScroll && currentScroll > 99) {
-    // down
-    // console.log('Last scroll ' + lastScroll);
-    navContainer.classList.remove('my-2');
-  } else if (currentScroll < lastScroll && currentScroll <= 99) {
-    // up
-    navContainer.classList.add('my-2');
+    const currentScroll = window.pageYOffset;
 
-  } 
+    if (currentScroll > lastScroll && currentScroll > 99) {
+        // down
+        // console.log('Last scroll ' + lastScroll);
+        navContainer.classList.remove('my-2');
+    } else if (currentScroll < lastScroll && currentScroll <= 99) {
+        // up
+        navContainer.classList.add('my-2');
 
-  lastScroll = currentScroll;
+    }
+
+    lastScroll = currentScroll;
 });
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+    } else {
+        document.getElementById("movetop").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
